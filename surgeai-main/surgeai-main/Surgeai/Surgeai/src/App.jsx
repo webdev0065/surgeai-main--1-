777f0682614be3317e310f0f64f7d5afdd1c229f
webdev0065 <webdev0065@gmail.com>
2025-05-20 12:36:34 +0530

    wdveab

--- surgeai-main/surgeai-main/Surgeai/Surgeai/src/App.jsx
+++ surgeai-main/surgeai-main/Surgeai/Surgeai/src/App.jsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 // import Cursor from './components/layout/Cursor';
 import Navbar from './components/layout/Navbar';
 import Hero from './components/sections/Hero';
